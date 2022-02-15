e23f26804f411fa55766e6fef2667774d8925bc7
ADVAIT RAJ SIRGA <91474902+AdvaitRajSirga@users.noreply.github.com>
2022-02-15 08:15:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
 let mailTransporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-      user: 'advaitrajsirga@gmail.com',
-      pass: 'chessplayer@2011'
+      user: 'attendenceflipp@gmail.com',
+      pass: 'ihateyouthemost'
   }
 });
 
@@ -38,8 +38,8 @@ console.log(print)
 
 
 let mailDetails = {
-  from: 'advaitrajsirga@gmail.com',
-  to: 'advaitrajsirga@gmail.com',
+  from: ' attendenceflipp@gmail.com',
+  to: ' attendenceflipp@gmail.com',
   subject: 'Attendance',
     text: "Details:" + " " + JSON.stringify(print),
     attachments: [{'filename': req.file.originalname,
